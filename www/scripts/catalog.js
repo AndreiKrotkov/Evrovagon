4c89f9f02f63f2496382eab88bf3757c9ea6879a
kam <kam@one-touch.ru>
2021-03-28 16:47:26 +0300

    final(верстка готова)

--- www/scripts/catalog.js
+++ www/scripts/catalog.js
@@ -12,4 +12,28 @@ $(document).ready(function() {
             $this.find('span').text('Смотреть все');
         }
     });
+
+
+    // var isSaidbarNav = $('.js-saidbar-nav').is(':visible');
+    // if (isSaidbarNav) {
+        $('.saidbar-nav__prev').on('click', function (e) {
+            e.preventDefault();
+            let $this = $(this);
+            console.log('хуй')
+            // let active = $this.closest('.saidbar-nav__item').hasClass('active-nav-saidbar');
+            $('.saidbar-nav__item').removeClass('active-nav-saidbar');
+            $this.closest('.saidbar-nav__item').addClass('active-nav-saidbar');
+
+            // $("#selectBackground ul li a").removeClass("active");
+            // $(this).toggleClass("active");
+
+            // if(!active) {
+            //     $this.closest('.saidbar-nav__item').addClass('active');
+            //     $this.find('span').text('Свернуть');
+            // } else {
+            //     $this.closest('.js-nav-elem').removeClass('active');
+            //     $this.find('span').text('Смотреть все');
+            // }
+        });
+    // }
 });
