9a534c5b7e406c6bd9603a0f1938db917e6ab23d
kam <kam@one-touch.ru>
2021-03-29 11:07:47 +0300

    fix-bug(acc, elem, anchor)

--- www/scripts/catalog.js
+++ www/scripts/catalog.js
@@ -18,9 +18,22 @@ $(document).ready(function() {
     if (isSaidbarNav) {
         $('.saidbar-nav__prev').on('click', function (e) {
             e.preventDefault();
-            let $this = $(this);
-            $('.saidbar-nav__item').removeClass('active-nav-saidbar');
-            $this.closest('.saidbar-nav__item').addClass('active-nav-saidbar');
+            let $this = $(this)
+            let active = $this.closest('.saidbar-nav__item').hasClass('active-nav-saidbar');
+            if(!active) {
+                $('.saidbar-nav__item').removeClass('active-nav-saidbar');
+                $this.closest('.saidbar-nav__item').addClass('active-nav-saidbar');
+            } else {
+                $this.closest('.saidbar-nav__item').removeClass('active-nav-saidbar');
+            }
         });
     }
+
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 220
+        }, 400);
+        return false;
+    });
 });
