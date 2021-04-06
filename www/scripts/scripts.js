350ac53eb5a36c508d8633ac12bedd5a2d75a6d3
kam <kam@one-touch.ru>
2021-04-06 23:10:17 +0300

    add-catalog-category(mobile)

--- www/scripts/scripts.js
+++ www/scripts/scripts.js
@@ -243,6 +243,52 @@ $(document).ready(function() {
         $this.removeClass('hover-active');
     });
 
+//    Каталог
+    $('.js-nav-elem__link').on('click', function (e) {
+        e.preventDefault();
+        let $this = $(this);
+        let active = $this.closest('.js-nav-elem').hasClass('active');
+
+        if(!active) {
+            $this.closest('.js-nav-elem').addClass('active');
+            $this.find('span').text('Свернуть');
+        } else {
+            $this.closest('.js-nav-elem').removeClass('active');
+            $this.find('span').text('Смотреть все');
+        }
+    });
+
+
+    var isSaidbarNav = $('.js-saidbar-nav').is(':visible');
+    // if (isSaidbarNav) {
+        $('.saidbar-nav__prev').on('click', function (e) {
+            e.preventDefault();
+            let $this = $(this)
+            let active = $this.closest('.saidbar-nav__item').hasClass('active-nav-saidbar');
+            if(!active) {
+                $('.saidbar-nav__item').removeClass('active-nav-saidbar');
+                $this.closest('.saidbar-nav__item').addClass('active-nav-saidbar');
+            } else {
+                $this.closest('.saidbar-nav__item').removeClass('active-nav-saidbar');
+            }
+        });
+    // }
+
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 220
+        }, 400);
+        return false;
+    });
+
+    // Каталог детальная
+    $('.js-nav-pre').on('click', function (e) {
+        e.preventDefault();
+        let $this = $(this);
+        $this.closest('.wrp__saidbar-nav').toggleClass('active');
+    });
+
 
 });
 
